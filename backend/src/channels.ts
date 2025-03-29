24f0c3183801e9f15965511cd0660ba15c612ff4
Jordan Denison <jordan@denisonweb.com>
2025-03-29 14:43:00 +0000

    impl url patch realtime

--- backend/src/channels.ts
+++ backend/src/channels.ts
@@ -1,11 +1,18 @@
 import type { Params } from '@feathersjs/feathers'
 import '@feathersjs/transport-commons'
 import type { Application } from './declarations'
+import { Url } from './services/urls/urls.schema'
 
 export const channels = (app: Application) => {
   app.on('login', async (_, { connection }: Params) => {
     if (connection && connection.user) {
-      app.channel(connection.user.id).join(connection)
+      app.channel(`${connection.user.id}`).join(connection)
+    }
+  })
+
+  app.service('urls').publish('patched', (data: unknown) => {
+    if (typeof data === 'object' && data !== null && 'userId' in data) {
+      return app.channel(`${(data as Url).userId}`)
     }
   })
 }
